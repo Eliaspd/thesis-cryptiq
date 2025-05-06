dcb337622d112751f7a2ce58f150d621b10a7d1c
Eliaspd <swedetailer@gmail.com>
2025-05-06 20:28:06 +0200

    Initial Commit

---
+++ Cryptiq/CryptiqApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptiqApp.swift
+//  Cryptiq
+//
+//  Created by Elias puolitaival on 2025-05-06.
+//
+
+import SwiftUI
+
+@main
+struct CryptiqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptiqTests/CryptiqTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptiqTests.swift
+//  CryptiqTests
+//
+//  Created by Elias puolitaival on 2025-05-06.
+//
+
+import Testing
+@testable import Cryptiq
+
+struct CryptiqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
